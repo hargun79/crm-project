dbcf5f422f34899d1c24b12ca709cd9ab2858000
Hargun Singh Sahni <hargunsinghsahni@gmail.com>
2022-07-14 14:20:46 +0530

    Initial Commit

---
+++ src/main/java/com/hargunsinghsahni/springdemo/service/CustomerService.java
@@ -0,0 +1,19 @@
+package com.hargunsinghsahni.springdemo.service;
+
+import java.util.List;
+
+import com.hargunsinghsahni.springdemo.entity.Customer;
+
+public interface CustomerService {
+
+	public List<Customer> getCustomers(int theSortField);
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+	public List<Customer> searchCustomers(String theSearchName);
+	
+}
